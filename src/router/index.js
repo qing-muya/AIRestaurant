2c1e7c1e869ae99763f349fb0fba044379ea3a90
木木 <2190825531@qq.com>
2021-08-18 19:23:02 +0800

    登录界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Index from '../views/Index.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter) // 注册路由，在注册时vue就自动生成了一个全局组件router-view
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/index',
+    name: 'Index',
+    component: Index
+  },
+  {
+    path: '*',
+    redirect: '/login'
   }
 ]
 
